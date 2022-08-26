9f0081de76e48708f879cceb044897be440c6762
aditya5901 <64686834+aditya5901@users.noreply.github.com>
2022-08-26 19:36:31 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -6,6 +6,11 @@ timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
 
 def create_spark_session(master="local[1]", appName = timestamp):
+    """
+    :param master: Provide master. Default local[1]
+    :param appName: Provide appname. Default current timestamp
+    :return: Returns a spark session
+    """
     # Enabling Hive to use in Spark
     spark = SparkSession.builder \
         .master(master) \
@@ -15,6 +20,10 @@ def create_spark_session(master="local[1]", appName = timestamp):
 
 
 def json_parser():
+    """
+    This functions reads a json file.
+    :return: Returns the object of json.load
+    """
     f = open('../config/file.json')
     data = json.load(f)
     return data
